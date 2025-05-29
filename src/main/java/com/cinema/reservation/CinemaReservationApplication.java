009411e8171806326669dff8fefc8e2df8acfd90
Wojciech Kapala <wokapala@gmail.com>
2025-05-29 23:10:47 +0200

    chore(config): update application properties and main class

--- src/main/java/com/cinema/reservation/CinemaReservationApplication.java
+++ src/main/java/com/cinema/reservation/CinemaReservationApplication.java
@@ -1,11 +1,17 @@
 package com.cinema.reservation;
 
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(
+        name   = "basicAuth",
+        type   = SecuritySchemeType.HTTP,
+        scheme = "basic"
+)
 public class CinemaReservationApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CinemaReservationApplication.class, args);
     }
